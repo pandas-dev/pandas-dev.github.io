b51128e89c58b54b0486f7c704cae7485b7fc712
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2026-07-23 10:45:24 +0200

    update pandas-docs/dev/ to latest build

--- pandas-docs/dev/_static/documentation_options.js
+++ pandas-docs/dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.1.0.dev0+1159.gd7b577e035',
+    VERSION: '3.1.0.dev0+1386.gcb2086a1a4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
